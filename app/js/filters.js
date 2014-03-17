9396ad0ee01ea311570a8492c9e3ee783df443b9
Daniel Margala <dmargala@uci.edu>
2014-03-17 01:52:39 -0700

    Add ordinal number filter, update member list view display

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,8 +2,32 @@
 
 /* Filters */
 
-angular.module('hercwebFilters', []).filter('checkmark', function() {
+var hercwebFilters = angular.module('hercwebFilters', []);
+
+hercwebFilters.filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
+  };
+});
+
+hercwebFilters.filter('ordinal', function() {
+  return function(number) {
+  	var numberString = number + '',
+  		ord = '';
+    switch (numberString.slice(-1)) {
+    	case '1':
+    		ord = numberString.slice(-2) === '11' ? 'th' : 'st';
+    		break; 
+    	case '2':
+    		ord = numberString.slice(-2) === '12' ? 'th' : 'nd';
+    		break;
+    	case '3':
+    		ord = numberString.slice(-2) === '13' ? 'th' : 'rd';
+    		break;
+    	default:
+    		ord = 'th';
+    		break;
+    }
+    return numberString + ord;
   };
 });
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -14,6 +14,22 @@ describe('filter', function() {
       expect(checkmarkFilter(false)).toBe('\u2718');
     }));
   });
+
+  describe('ordinal', function() {
+
+  	it('should convert integer values to ordinal representation',
+  		inject(function(ordinalFilter) {
+  	  expect(ordinalFilter(1)).toBe('1st');
+  	  expect(ordinalFilter(2)).toBe('2nd');
+  	  expect(ordinalFilter(3)).toBe('3rd');
+  	  expect(ordinalFilter(4)).toBe('4th');
+  	  expect(ordinalFilter(22)).toBe('22nd');
+  	  expect(ordinalFilter(43)).toBe('43rd');
+  	  expect(ordinalFilter(11)).toBe('11th');
+  	  expect(ordinalFilter(12)).toBe('12th');
+  	  expect(ordinalFilter(13)).toBe('13th');
+  	}));
+  });
 });
 
 
